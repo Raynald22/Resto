918d39e430c6492370c329a6dadbc80d03f032e0
Raynald22 <44433953+Raynald22@users.noreply.github.com>
2020-02-03 05:37:20 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('model_produk', 'model_pegawai');
+$autoload['model'] = array('model_produk', 'model_pegawai', 'model_pesanan');
