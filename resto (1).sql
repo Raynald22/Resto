d238dd5ad3d60608defbb577c41fb7e6d6bdebd8
Raynald22 <44433953+Raynald22@users.noreply.github.com>
2020-02-03 15:00:32 +0700

    Add files via upload

---
+++ resto (1).sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 03, 2020 at 09:00 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `resto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` int(11) NOT NULL,
+  `no_meja` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `harga` int(125) NOT NULL,
+  `gambar` text NOT NULL,
+  `status` enum('Ready','Unavailable') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`id`, `no_meja`, `id_produk`, `nama`, `jumlah`, `harga`, `gambar`, `status`) VALUES
+(2, 1, 0, 'Rendang', 1, 15000, '', 'Ready'),
+(4, 2, 0, 'Nasi Uduk', 1, 150000, '', 'Ready');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `kategori` varchar(30) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `gambar` text NOT NULL,
+  `status` enum('Ready','Unavailable') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama`, `kategori`, `harga`, `gambar`, `status`) VALUES
+(20200202, 'Rendang', 'Makanan', 15000, 'rendang.jpg', 'Ready');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(4, 'Reynaldi Fikri Hidayat', 'inyourd7@gmail.com', 'default.jpg', '$2y$10$5HErqdJzaQyM2WYBEotFXOjxBQYuyQNO.5/6mXrdwreac9WkMBeg6', 2, 1, 1580650564),
+(5, 'Sera Vina', 'seravina@gmail.com', 'default.jpg', '$2y$10$dCkgcdDl31rI2wEWh0wWq.dMy/kEttXmDQgI0qmNMwRmparYyPa9i', 1, 1, 1580651279),
+(6, 'Pelayan', 'pelayan@gmail.com', 'default.jpg', '$2y$10$7MAovn8J9wyErPMYH1bH.OQJ8y5y8f4Msxi6hcyeto8qUodVeyx8q', 3, 1, 1580651417);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20200204;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
