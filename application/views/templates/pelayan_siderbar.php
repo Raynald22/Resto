4eebfd49e001e1898e07c3aa74066cf80172558d
Raynald22 <inyourd7@gmaill.com>
2020-02-03 14:43:43 +0700

    Perubahan

--- application/views/templates/pelayan_siderbar.php
+++ application/views/templates/pelayan_siderbar.php
@@ -92,8 +92,8 @@
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['name']; ?></span>
-                                <img class="img-profile rounded-circle" src="<?= base_url('assets/img/') . $user['image']; ?>">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Pelayan</span>
+                                <img class="img-profile rounded-circle" src="<?= base_url('assets/img/default.jpg') ?>">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
